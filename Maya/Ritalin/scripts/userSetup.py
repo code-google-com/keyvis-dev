47f97eeffd139d87ee35be322becae3e8b61a696
stefan@keyvis.at <stefan@keyvis.at@a75dab56-0351-11df-afd9-7791053e64b7>
2010-08-18 19:47:45 +0000

    Updated Ritalin to version 1.5 which fixes a Maya 2011 bug

--- Maya/Ritalin/scripts/userSetup.py
+++ Maya/Ritalin/scripts/userSetup.py
@@ -1,4 +1,2 @@
-#Put this file in your user scripts folder, or, in case it already exists, copy the following line into it.
-#Make sure to adjust the path according to your actual user name, folder location and Maya version.
-
+#Put this file in your user scripts folder, or, in case it already exists, copy the following line into it:
 execfile ("C:\\Documents and Settings\\Administrator\\My Documents\\maya\\2009-x64\\scripts\\Ritalin_Public.py")
