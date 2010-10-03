22328aa0e28818f07fbc1c03adce401ea5af3d09
stefan@keyvis.at <stefan@keyvis.at@a75dab56-0351-11df-afd9-7791053e64b7>
2010-10-03 21:25:17 +0000

    Updated VS startup batch files

--- Softimage/VC64XSIx64.bat
+++ Softimage/VC64XSIx64.bat
@@ -1,11 +1,12 @@
 @echo off
 
-call "C:\Softimage\XSI_7.01_x64\Application\bin\Setenv.bat"
+call "C:\Program Files\Autodesk\Softimage 2011 SP1\Application\bin\Setenv.bat"
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" amd64
 
 set PATH
-set XSISDK_ROOT="C:\Softimage\XSI_7.01_x64\XSISDK"
+set XSISDK_ROOT="C:\Program Files\Autodesk\Softimage 2011 SP1\XSISDK"
 
 "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" /useenv
 
 echo on
+rem pause
